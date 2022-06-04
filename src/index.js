8ece8e9da167676f0acd4e2d6a094f924adaa454
vaibhav <vaibhavkumarjha46@gmail.com>
2022-06-05 02:03:16 +0530

    Added Nav Bar & Sign-in and Sign-up page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+import Header from "./common/header/Header";
+import Home from "./components/Home/Home"
+import SignIn from "./components/SignPage/SignIn";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  // <React.StrictMode>
+  <div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path ="/signIn" element={<SignIn/>}/>
+      </Routes>
+    </Router>
+  </div>,
+  // </React.StrictMode>
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
